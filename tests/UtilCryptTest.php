94610cb8d7ee89c2b6ad64b85d14759abb1fd850
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2023-01-03 16:11:28 +0100

    Add tests

---
+++ tests/UtilCryptTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Conia\Crypt\Crypt;
+
+
+test('Encryption and decryption', function () {
+    expect(Crypt::decrypt(
+        Crypt::encrypt('Symbolic', 'secret-key'),
+        'secret-key'
+    ))->toBe('Symbolic');
+});
+
+
+test('Encryption and decryption with alternate algo', function () {
+    expect(Crypt::decrypt(
+        Crypt::encrypt('Symbolic', 'secret-key', 'aes-256-cbc'),
+        'secret-key',
+        'aes-256-cbc'
+    ))->toBe('Symbolic');
+});
+
+
+test('Failing encryption', function () {
+    Crypt::encrypt('Symbolic', 'secret-key', 'wrong-algo');
+})->throws(ValueError::class, 'Cipher algorithm');
+
+
+test('Failing decryption', function () {
+    Crypt::decrypt('Symbolic', 'secret-key', 'wrong-algo');
+})->throws(ValueError::class, 'Cipher algorithm');
